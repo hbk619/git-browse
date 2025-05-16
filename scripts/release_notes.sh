1fea4b3c81769e31c35abba72cdcef41cfe0ed0b
hbk619 <hbk619@users.noreply.github.com>
2025-05-16 10:33:49 +0100

    chore(build): add @ to author in release notes
    So that Github links to them

--- scripts/release_notes.sh
+++ scripts/release_notes.sh
@@ -8,7 +8,7 @@ pr_regex="/#[0-9]+/"
 while IFS=£ read -r commit_body author subject; do    
     if [[ $subject =~ Merge\ pull\ request\ #([0-9]+) ]]; then
         pr_number=${BASH_REMATCH[1]}
-        echo "* $commit_body by $author in https://github.com/hbk619/gh-peruse/pull/$pr_number"
+        echo "* $commit_body by @$author in https://github.com/hbk619/gh-peruse/pull/$pr_number"
     fi
 done <<< "$(git log --merges --pretty=format:"%b£%an£%s" "$latest_tag"..HEAD)" 
 
